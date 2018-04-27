4b765c7e71d4b1f542a00273e3c374d629a9e4ae
Milo Wical <Milo.Wical@gmail.com>
2018-04-27 11:16:48 -0400

    Updated project so that the necessary environment variable can be read and reset.

--- DockerTest/EnvironmentVariableTest/Program.cs
+++ DockerTest/EnvironmentVariableTest/Program.cs
@@ -2,11 +2,27 @@
 
 namespace EnvironmentVariableTest
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Beginning property dump...\n");
+
+            foreach(string key in Environment.GetEnvironmentVariables().Keys)
+            {
+                Console.WriteLine($"Environment Key: {key}, Value: {Environment.GetEnvironmentVariable(key)}");
+            }
+
+            Console.WriteLine("Changing value for 'TestKey'...\n");
+
+            Environment.SetEnvironmentVariable("TestKey", "NewKeyValue");
+
+            foreach (string key in Environment.GetEnvironmentVariables().Keys)
+            {
+                Console.WriteLine($"Environment Key: {key}, Value: {Environment.GetEnvironmentVariable(key)}");
+            }
+
+            Console.WriteLine("\nDump completed.");
         }
     }
 }
